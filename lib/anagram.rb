506842701575f4d7fa2e18c795c36d1499d4efa7
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-08 03:46:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(anagrams)
+  end 
+    
+end 
+
+listen = Anagram.new("listen")
+listen.match(%w(hello world zombies pants dipper))
