8bf6d0dcb04ec8193e5a8872c9a07a7565812b84
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-08 03:51:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,4 @@
-class Anagram
-  attr_accessor :word
 
-  def initialize(word)
-    @word = word
-  end
 
-  def match(words)
-    words.select do |element|
-      (@word.split("").sort) == (element.split("").sort)
-    end 
-  end
-end
+listen = Anagrams.new("listen")
+listen.match(%w(hello world zombies pants dipper))
